0effd911211beb0c5f934c18bf95369cdb914918
NILESH D <cricketlife224@gmail.com>
2022-04-25 12:18:08 +0530

    Add project files.

---
+++ MVC_eCommerce_Ashley_Furniture_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_eCommerce_Ashley_Furniture_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
